e1dc97db0c75613859c756168295c951b9b5b68a
magatdarwin <magat.darwin@gmail.com>
2023-03-17 16:25:26 +0800

    Create Book object

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.apges = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
