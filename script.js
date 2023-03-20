48577ba7ec43f32852b42786b5f9594ed8e74236
magatdarwin <magat.darwin@gmail.com>
2023-03-20 18:04:17 +0800

    Add show and hide modal functionality

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function Book(title, author, pages, readStatus) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+function showBookFormModal() {
+  bookFormModal.style.display = 'block';
+  console.log(bookFormModal);
+}
+
+function hideBookFormModal(event) {
+  if (event.target === bookFormModal || event.target === cancelButton) {
+    bookFormModal.style.display = 'none';  
+  }
+}
+
+const bookFormModal = document.querySelector('#book-form-modal');
+const addBook = document.querySelector('button.add');
+const cancelButton = document.querySelector('input[value="Cancel"]');
+
+addBook.addEventListener('click', showBookFormModal);
+cancelButton.addEventListener('click', hideBookFormModal);
+window.addEventListener('click', hideBookFormModal);
